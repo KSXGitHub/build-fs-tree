4921c55d5d0a79a7748214fd1a188f13d6290664
khai96_ <hvksmr1996@gmail.com>
2021-04-09 13:34:32 +0700

    test: Add more test cases to build.rs

--- test/build.rs
+++ test/build.rs
@@ -3,10 +3,21 @@ use crate::{sample_tree, temp_workspace, test_sample_tree};
 use build_fs_tree::Build;
 use std::path::PathBuf;
 
-#[test]
-fn string_string() {
-    let _temp = temp_workspace().unwrap();
-    let target = PathBuf::from("root");
-    sample_tree::<String, String>().build(&target).unwrap();
-    test_sample_tree(&target);
+macro_rules! test_case {
+    ($name:ident, $root:expr, $key:ty, $value:ty $(,)?) => {
+        #[test]
+        fn $name() {
+            let temp = temp_workspace().unwrap();
+            let target = PathBuf::from(temp.join($root));
+            sample_tree::<$key, $value>().build(&target).unwrap();
+            test_sample_tree(&target);
+        }
+    };
 }
+
+test_case!(string_string, "root", String, String);
+test_case!(str_slice_string, "root", &'static str, String);
+test_case!(string_str_slice, "root", String, &'static str);
+test_case!(str_slice_str_slice, "root", &'static str, &'static str);
+test_case!(path_buf_str_slice, "root", PathBuf, &'static str);
+test_case!(path_buf_u8_vec, "root", PathBuf, ::std::vec::Vec<u8>);
