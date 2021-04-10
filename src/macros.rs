3d2322ddd7e2fa7017e680c5a1929d00412ff84b
khai96_ <hvksmr1996@gmail.com>
2021-04-10 13:36:14 +0700

    doc: Add examples to dir!

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,70 @@
 #![no_implicit_prelude]
 
 /// Create representation of a [directory](crate::FileSystemTree::Directory).
+///
+/// **Example:** Without commas
+///
+/// ```
+/// use build_fs_tree::{FileSystemTree, dir, file};
+///
+/// let tree: FileSystemTree<&str, &str> = dir! {
+///     "a" => file!("foo")
+///     "b" => file!("bar")
+///     "c" => dir! {
+///         "x" => file!("baz")
+///     }
+/// };
+///
+/// # dbg!(&tree);
+/// assert_eq!(
+///     tree.dir_content().unwrap()
+///         .get("a").unwrap().file_content().unwrap(),
+///     &"foo",
+/// );
+/// assert_eq!(
+///     tree.dir_content().unwrap()
+///         .get("b").unwrap().file_content().unwrap(),
+///     &"bar",
+/// );
+/// assert_eq!(
+///     tree.dir_content().unwrap()
+///         .get("c").unwrap().dir_content().unwrap()
+///         .get("x").unwrap().file_content().unwrap(),
+///     &"baz",
+/// );
+/// ```
+///
+/// **Example:** With commas
+///
+/// ```
+/// use build_fs_tree::{FileSystemTree, dir, file};
+///
+/// let tree: FileSystemTree<&str, &str> = dir! {
+///     "a" => file!("foo"),
+///     "b" => file!("bar"),
+///     "c" => dir! {
+///         "x" => file!("baz"),
+///     },
+/// };
+///
+/// # dbg!(&tree);
+/// assert_eq!(
+///     tree.dir_content().unwrap()
+///         .get("a").unwrap().file_content().unwrap(),
+///     &"foo",
+/// );
+/// assert_eq!(
+///     tree.dir_content().unwrap()
+///         .get("b").unwrap().file_content().unwrap(),
+///     &"bar",
+/// );
+/// assert_eq!(
+///     tree.dir_content().unwrap()
+///         .get("c").unwrap().dir_content().unwrap()
+///         .get("x").unwrap().file_content().unwrap(),
+///     &"baz",
+/// );
+/// ```
 #[macro_export]
 macro_rules! dir {
     ($($key:expr => $value:expr $(,)?)*) => {{
