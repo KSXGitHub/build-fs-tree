a49449baf8291d1c3edf9778a5723d86837a628e
khai96_ <hvksmr1996@gmail.com>
2023-06-12 01:26:58 +0700

    doc: RuntimeError

--- src/program/main/error.rs
+++ src/program/main/error.rs
@@ -2,8 +2,11 @@ use crate::BuildError;
 use derive_more::{Display, Error, From};
 use std::{fmt::Debug, io};
 
+/// Error when execute the [`run`](super::run::run) function.
 #[derive(Debug, Display, From, Error)]
 pub enum RuntimeError<Path> {
+    /// Failed to parse YAML from stdin.
     Yaml(serde_yaml::Error),
+    /// Failed to create the filesystem tree.
     Build(BuildError<Path, io::Error>),
 }
